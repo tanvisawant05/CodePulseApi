5fa9e530a9f0c878af1c9175538b0dbd4a9b191a
tanvisawant05 <tanvisawant2001@gmail.com>
2023-12-06 08:40:15 +0530

    Initial Commit

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Implementation;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+       private readonly ICategoryRepository categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody]CreateCategoryRequestDto request)
+        {
+            //Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+            
+            await categoryRepository.CreateAsync(category);
+            //Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+            //Map domain model to DTO
+            var response = new List<CategoryDto>();
+            foreach(var category in categories)
+            {
+                response.Add(new CategoryDto
+                { Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle});
+
+            }
+            return Ok(response);
+        }
+        //GET: https://localhost:7136/api/categories/{id}
+
+        
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] Guid id)
+        {
+            var existingCategory = await categoryRepository.GetById(id);
+            if (existingCategory is null)
+            {
+                return NotFound();
+            }
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+            return Ok(response);
+        }
+    
+    }
+}
---
+++ CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext=dbContext;
+           
+        }
+
+        public ApplicationDbContext DbContext { get; }
+
+        internal static Task GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await dbContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetById(Guid id)
+        {
+           return await dbContext.Categories.FirstOrDefaultAsync(x=>x.Id ==id);
+        }
+    }
+}
